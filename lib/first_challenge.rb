a3f06332ec4c384e2ac7b28705b1c002bc371da7
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 16:17:17 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,9 @@
+#-- Iterate over the contacts hash and when you reach the key of :favorite_icecream_flavors, remove "strawberry" from the array of Freddy's favorite ice cream flavors. There are at least two ways you can accomplish this:
+#-* 1) You can iterate through the hash and, when you reach the appropriate level, check to see if the key == ("is equal to") :favorite_icecream_flavors. If it does, check to see if that array contains "strawberry". If it does, then delete it from the array.
+#-* OR 2) you can directly iterate over the hash that is the value of the "Freddy Mercury" key by calling an enumerator method in contacts["Freddy Mercury"].
+#--- Hint: Use the .delete_if method to eliminate strawberry from the appropriate array.
+#--- Hint: Remember that the first_challenge method needs to return the newly altered contacts hash.
+
 def first_challenge
   contacts = {
     "Jon Snow" => {
@@ -14,7 +20,15 @@ def first_challenge
   }
 
   #your code here
-
+  #contacts.each do |person, data|
+    
+  contacts["Freddy Mercury"].each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+        value.delete_if do |flavor|
+          flavor == "strawberry"
+        end
+      end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
