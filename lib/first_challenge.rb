3401ba9d124cf066df34eee47a4cace9aa8d30ef
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 16:11:48 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -26,6 +26,9 @@ def first_challenge
       if attribute == :favorite_icecream_flavors
         value.delete_if do |flavor|
           flavor == "strawberry"
+        end
+      end
+    end
 
   #remember to return your newly altered contacts hash!
   contacts
